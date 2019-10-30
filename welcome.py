a9a17900d24cc14e00b6bccdc813e1f628ffc383
Hub972 <hassouvie@gmail.com>
2019-10-30 12:55:54 +0100

    change travis conf

--- welcome.py
+++ welcome.py
@@ -1,1 +1,6 @@
-print("Hello world")
+esp = '##########################'
+print(f'{esp}hello world{esp}\n The purpose of this app is to help the consumer and the farmer to get \
+over the distribution chains.\n The farmer must register first and then \
+contact the sysadmin to become a staff.\n You can visit my site to \
+163.172.178.97\n{esp}           {esp}')
+
